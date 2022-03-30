13d50afed1f5eff2e1db4bfaa7b0c5248845ca45
atjpa <caoducan351@gmail.com>
2022-03-30 10:19:01 +0700

    Cài đặt trang lỗi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
         name: "ContactBook",
         component: ContactBook,
     },
+    {
+        path: "/:pathMath(.*)*",
+        name: "NotFound",
+        component: () => import("@/views/NotFound.vue"),
+    },
 ];
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page">
+      <p>
+          Oops, không thế tìm thấy trang. Trở về
+          <router-link to="/"> Trang chủ</router-link>
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
